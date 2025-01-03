bb2a68ea36cb2d272dc0ce752cce3dffb587f02d
juanjose <juanjoperez2121@gmail.com>
2025-01-03 18:41:30 -0500

    the first commit that included header and home, structure and style

---
+++ script.js
@@ -0,0 +1,8 @@
+const title = document.getElementById("title--name");
+const textos = ["Programador", "Frontend"];
+let index = 0;
+
+setInterval(() => {
+  title.innerText = textos[index];
+  index = index < textos.length - 1 ? index + 1 : 0;
+}, 5000);
